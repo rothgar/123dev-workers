49b007b95f987075d5e1a8abb34246a065735034
Justin Garrison <justinleegarrison@gmail.com>
2021-05-10 23:43:32 -0700

    working v1

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 async function handleRequest(request) {
   // Return a new Response based on a URL's hostname
-  const url = new URL(request.url)
-  // redirect twitterbot
-  const userAgent = request.headers.get("User-Agent") || ""
-  console.log(userAgent)
-  if (userAgent.includes("twitterbot")) {
-    console.log("matched twitterbot")
-    var re = /posts/gi
-    console.log(url.replace(re, "embed"))
-    return Response.redirect(url.replace(re, "embed"))
+  let url = new URL(request.url);
+  url = `${url}`;
+  const statusCode = 301
+  
+  const { searchParams } = new URL(request.url)
+  let auto_play = searchParams.get('auto_play')
+
+  if (auto_play) {
+    // replace 123dev.email/posts/1 with 123dev.email/embed/posts/1
+    var re = /embed/gi
+    console.log(`returning ${url}`)
+    return fetch(url.replace(re, "embed/posts"));
   }
 
-  return fetch(request)
+  // redirect twitterbot
+  const userAgent = request.headers.get("User-Agent") || ""
+  if (userAgent.includes("Twitterbot")) {
+    if (request.url.includes("posts") && !request.url.includes("embed")) {
+      //replace 123dev.email/posts/1 with 123dev.email/embed/posts/1
+        var re = /posts/gi
+        console.log(`Redirecting twitterbot to ${url.replace(re, "embed/posts")}`)
+        return Response.redirect(url.replace(re, "embed/posts"), statusCode)
+    } else if (request.url == "https://123dev.email/") {
+      console.log(`Shared home page`)
+      return Response.redirect("https://123dev.email/embed/posts/1/", statusCode)
+    }
+  }
+  if (request.url.includes("embed")) {
+    if (!userAgent.includes("Twitterbot")) {
+      //replace 123dev.email/embed/posts/1 with 123dev.email//posts/1
+      var re = /embed/gi
+      console.log(`Redirecting user to ${url.replace(re, "")}`)
+      return Response.redirect(url.replace(re, ""), statusCode)
+    }
+  }
+  console.log(`returning ${url}`)
+  return fetch(url);
 }
 
 addEventListener("fetch", event => {
   // NOTE: can’t use fetch here, as we’re not in an async scope yet
+  console.log(`Received new request: ${event.request.url}`)
   event.respondWith(handleRequest(event.request))
 })
